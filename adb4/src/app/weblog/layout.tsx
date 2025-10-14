163355abcd6d3cb1c7424910e8e88b4034e28551
andydbui <buiand@fastmail.com>
2025-10-13 22:36:00 -0500

    hehe

--- adb4/src/app/weblog/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function WeblogLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <section>{children}</section>
-}
--- adb4/src/app/weblog/page.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-
-export default function WeblogComponent() {
-    return (
-        <div className="home-container">
-            weblog
-        </div>
-    )
-}
