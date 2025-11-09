66e58f9ea8960e642ef74f5638234c7a3094a218
andydbui <buiand@fastmail.com>
2025-11-08 22:57:10 -0600

    hm

--- components/home.tsx
+++ components/home.tsx
@@ -5,7 +5,7 @@ import { useTheme } from 'next-themes'
         
 import TypewriterComponent from './typewriter';
 
-const comingsoonmessage: string = "Friend, I've moved to Austin, Texas for work, and am working on several updates for this domain. Stay tuned.";
+const comingsoonmessage: string = "Friend, I've moved to Austin, Texas for work, and am putting together several updates for this domain. Stay tuned.";
 
 const copy: string = "A programmer and 3D artist based in Austin, Texas with a penchant for designing functional, responsive interfaces, for which he cut his teeth in the olden days of MyBB forums. He is a Michigan native and an alumnus of the University of Michigan.  ";
 
